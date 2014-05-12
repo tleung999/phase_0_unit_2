5f173cfb4e1b04306c52cd6fe0ab866dbd90ab68
Tony Leung <tleung999@gmail.com>
2014-05-12 14:27:26 -0700

    submit pseudo code for u2w4 add it up

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -4,22 +4,25 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
 # What is the input?
 # What is the output? (i.e. What should the code return?)
 # What are the steps needed to solve the problem?
+# Create a new new method called total
+# Allow the new method called total to accept 1 array as an argument
+# Calculate the sum of the array and return the sum
+# Create another method called sentence_maker
+# Allow the new method called sentence_maker to accept 1 array as an argument
+# Join all the array elements together to become 1 long sentence.
+# Capitalize the first letter in the sentence
+# Place a period at the end of the sentence.
 
 
 # 2. Initial Solution
 
-
-
-
 # 3. Refactored Solution
-
-
 
 # 4. Reflection 
