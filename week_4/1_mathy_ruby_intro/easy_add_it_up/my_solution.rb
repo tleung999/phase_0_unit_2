30b666431e099d0c7298d8f869347c0efea28fae
Tony Leung <tleung999@gmail.com>
2014-05-12 14:50:44 -0700

    submit reflection for u2w4 add it up

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -19,6 +19,7 @@
 # Join all the array elements together to become 1 long sentence.
 # Capitalize the first letter in the sentence
 # Place a period at the end of the sentence.
+# Return sentence
 
 
 # 2. Initial Solution
@@ -51,3 +52,21 @@ def sentence_maker array
 end
 
 # 4. Reflection 
+=begin
+Having a clear understanding of the problem was absolutely key in building my pseudo code and everything else down the line
+I had to read the directions at least 4 times before I was exactly sure of what they want.   You really dont want 
+to miss anything, else you need to rewrite the pseudo code again. I tried my best to be clear on what methods to create, 
+how many arguements, what you want to achieve with the data being pushed into the method.   
+
+I had some slight problems remembering the exact syntax in Ruby since we've been working on Javascript the past week
+and I intermingled the syntax.    Also I realized that if I didnt declare a new varible as something and start using it
+in the array iteration, it will error out.    But once I figured it out, things started to come back to me.   In my initial 
+solution, I was pretty crude in devising a way to add the array and contatenate the array.   I broke up the pieces such as shifting
+out the first array element, capitalizing that, poping out the last array element and adding a period to it, contatenate everything
+in between and then finally put it all together.   Ruby-doc.org really helped bring back how to write the syntax.
+
+During the refactor solution part, I recalled the enumerable class and it has a lot of short hand ways of doing thing.  So 
+I went there to look up some ways to simplify things.   I tried to challenge myself by making the method only one line long
+and returning the results all at once.   It was really fun to see it done so easily.    I didnt find the challenge tedious,
+the challenge was straight forward and clear on exactly what we needed to do.   
+=end
