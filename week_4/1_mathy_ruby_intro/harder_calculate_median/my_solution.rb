2e4e19f8087e12c0fde99813c0aff560847c6362
Tony Leung <tleung999@gmail.com>
2014-05-12 15:19:15 -0700

    add pseudocode to u2w4 calculate the median

--- week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
+++ week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
@@ -4,13 +4,23 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
-
-# What is the input?
+# What is the input? 
+#Looks like it is taking in an array
 # What is the output? (i.e. What should the code return?)
+#Shoudl return the median from an array set
 # What are the steps needed to solve the problem?
+#Create a method called median which can take in 1 array argument
+#Sort array in ascending order (smallest to largest)
+#Check the length of the array
+#If the array is an odd length 
+# => calculate median by getting the middle array value
+#if the array is an even length
+# => calculate median by getting the two middle array value
+# => Get the total of both values together
+# => Take the total and divide by 2
 
 
 # 2. Initial Solution
