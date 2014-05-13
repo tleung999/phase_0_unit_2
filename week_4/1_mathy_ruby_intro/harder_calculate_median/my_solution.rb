083291b135201def72fea36528577d430b44450c
Tony Leung <tleung999@gmail.com>
2014-05-12 21:39:50 -0700

    added Refactored and Reflection

--- week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
+++ week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
@@ -37,7 +37,27 @@ def median array
 end
 
 # 3. Refactored Solution
-
+def median array
+	sortarr = array.sort
+	if array.length % 2 == 0 then
+		val1 = sortarr[array.length/2-1].to_f
+		val2 = sortarr[array.length/2].to_f
+		return (val1 + val2)/2
+	else
+		return sortarr[(array.length/2).ceil]
+	end
+end
 
 
 # 4. Reflection 
+=begin
+It was pretty easy to layout my pseudo code and figure out what method
+I needed to make plus how many parameters to pass.    I had to look online
+to see what the formula was to calculate the median when the array length was
+even.    One of the things I learned was chaining methods, but then I realize it
+was bad practice since it made my code so unreadable.    I ended up break it up
+into easy to read chunks so people can clearly see that for even array length
+you take the middle value 1 and middle val2, add them together and divide by 2.
+I am pretty confident with this challenge, it was really fun to solve.  Nothing
+tedious with this project, it was clean and easy to learn.	
+=end
