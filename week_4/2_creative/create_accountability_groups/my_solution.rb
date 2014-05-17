54bdc218f8c9897a9d48532a0e902d2395ef228c
Tony Leung <tleung999@gmail.com>
2014-05-16 21:18:58 -0700

    complete challenge 2 and 6

--- week_4/2_creative/create_accountability_groups/my_solution.rb
+++ week_4/2_creative/create_accountability_groups/my_solution.rb
@@ -10,23 +10,19 @@
 # Steps:
 =begin
 Create a method called Cohort that will take in an array as an argument
-Take the array and randomize the list and save it as unit1list
+If the total number of students are divisible by 4, we have the exact number needed to fill all slots evenly
+	Calculate the number of groups we need by taking the number of students divided by 4
+If not divisible by 4, we have extra students, so create an extra group to stick them in.
+Take the array list and shuffle it
 Break up the array into groups of 4
-Display the list of students
-Any extra students left over becomes their own group.
-Take the array and randomize the list again and save it as unit2list
-Break up the array into groups of 4
-Any extra students left over becomes their own group.
-Display the list of students
-Take the array and randomize the list again and save it as unit3list
-Break up the array into groups of 4
-Any extra students left over becomes their own group.
-Display the list of students
+Create a hash list to save the groups.   Each key (group#) has an array of students.
+Run a loop as many times as the number of groups.  So if total groups are 6, loop it 6 times.
+	Inside each loop iteration, insert the array of students into each group key.
+Return the final group list as a hash back to the caller.
 =end
 
 # 3. Initial Solution
 students = ["Ariel Fogel", "Hethe Berg", "Premila Anand", "Brian Junio", "John Akers", "Chandler Smith", "Gannon Curran", "Tony Leung", "Arik Gadye", "Allison Reilly", "Justin Harnoss", "Morgan O'Leary", "Puru Dahal", "Alex White", "Michele Murphy", "Matthew Darin", "Steven Leu", "Patrick Reynolds", "Lauren Kroner", "Albert Alquisola", "Ernie Salazar", "Jared K. Chevalier", "William Pangestu"]
-
 =begin
 def Cohort array
 	if array.length % 4 == 0
@@ -35,98 +31,89 @@ def Cohort array
 		totalgrp = array.length/4 + 1
  	end
 
-	unit1list = array.shuffle
-	unit2list = array.shuffle
-	unit3list = array.shuffle
+	arrshuf = array.shuffle
+	grplist = arrshuf.each_slice(4)
 	
-	slicelist1 = unit1list.each_slice(4)
-	slicelist2 = unit2list.each_slice(4)
-	slicelist3 = unit3list.each_slice(4)
+	finallist = {}
 
 	i = 1
-	puts "Unit 1 List"
 	while (totalgrp >= i)
-		puts "Group#{i}"
-		puts slicelist1.next
-		puts "*******************"
+		finallist["Group#{i}"] = grplist.next.to_a
 		i += 1
 	end
-
-	i = 1
-	puts
-	puts "Unit 2 List"
-	while (totalgrp >= i)
-		puts "Group#{i}"
-		puts slicelist2.next
-		puts "*******************"
-		i += 1
-	end
-
-	i = 1
-	puts
-	puts "Unit 3 List"
-	while (totalgrp >= i)
-		puts "Group#{i}"
-		puts slicelist3.next
-		puts "*******************"
-		i += 1
-	end
-
+	finallist
 end
 
-Cohort(students)
-
+puts "Unit 1 List"
+puts Cohort(students).inspect
+puts "**********************"
+puts "Unit 2 List"
+puts Cohort(students).inspect
+puts "**********************"
+puts "Unit 3 List"
+puts Cohort(students).inspect
+puts "**********************"
 =end
+
 
 # 4. Refactored Solution
 def Cohort array
-	#get the number of groups to make
-	if array.length % 4 == 0
-		totalgrp = array.length/4
-	else
-		totalgrp = array.length/4 + 1
- 	end
- 	
- 	#iterate 3 times to create 3 total units
-  unit = 1
-	3.times do
-		#shuffle the array and slice it up into chunks of 4 per group
-		slicelist = array.shuffle.each_slice(4)
-		puts "Unit #{unit} List" #display unit name
-		i = 1
-		#output each sliced chunk of 4 members without throwing a StopIteration error
-		while (totalgrp >= i)	
-			puts "Group#{i}" 	#display group name
-			puts slicelist.next #output each group of 4s
-			puts "*******************"
-			i += 1 #move the next group
-		end
-		puts
-		unit += 1 #move to next unit
+	finallist = {}
+ 	gpnum =  1
+	array.shuffle.each_slice(4) do |curslice|
+		finallist["Group#{gpnum}"] = curslice.to_a
+		gpnum += 1
 	end
+	finallist
 end
 
-Cohort(students)
-
+puts "Unit 1 List"
+puts Cohort(students).inspect
+puts "**********************"
+puts "Unit 2 List"
+puts Cohort(students).inspect
+puts "**********************"
+puts "Unit 3 List"
+puts Cohort(students).inspect
+puts "**********************"
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
-#Is Cohort a Method
+#Is Cohort a Method and can it take 1 array argument
 if Cohort(students) != nil
 	puts true
 end
-#Does Cohort take 1 array arguement
-if Cohort(students, students) == nil
+
+#Does the Cohort Method return a hash?
+puts Cohort(students).is_a?(Hash)
+
+#Does each group only have a max of 4 people
+hash = Cohort(students)
+chk = false
+hash.each_value do |val|
+	if val.length <= 4
+		chk = true
+	end
+end
+puts chk
+
+#Does a list of 23 names create 6 groups total to fill?
+if hash.length == 6
 	puts true
 end
-#Does each group only have a max of 4 people
-
-#Does the Cohort class create 3 result list
-
-
-
-
-
 
 # 5. Reflection 
+=begin
+This challenge took the longest time since I had a really hard time trying to get the list of 23 names to be unique each time up to 
+3 units.    I could get the first and second unit unique so that no students can group with each other again, but the third unit
+no matter how I swapped the names, I could not make it unique.   I ended up just shuffling the array and calling the Cohort method
+3 times to return a hash containing a shuffled version of each unit list.   Some other problems I faced was I did not know exactly how to
+handle each_slice(#).   I didnt know how to use the block correctly so i ended up just assigning a variable without a block which
+returned an enum.   The enum in the beginning I didnt know how to use until I found an example online which told me I had to call the
+enum and use .next to call each piece of the 4 person array chunk.   That was why in my initial solution I had to keep track of how
+many groups I needed so I can cycle through a while statement and keep calling .next till the iterator is equal to the 
+total number of groups.   If I did not do that, it will return a stopIteration error.  My refactored version is much cleaner after 
+I figured out how to build a block after the each_slice(x) method call.   
 
-
+So in this challenge, i learned how to use the each_slice, each_value, and is_a method.   This challenge was the toughest out of
+all the other challenges this week.   I spent two evenings (over 5 hours) pseudo coding the different permutations with no luck.   
+=end
--- week_4/6_cipher_challenge/my_solution.rb
+++ week_4/6_cipher_challenge/my_solution.rb
@@ -1,16 +1,77 @@
 # U2.W4: Cipher Challenge
 
-
-# I worked on this challenge with: .
-
-
+# I worked on this challenge with: John Akers.
 
 # 1. Solution
 # Write your comments on what each thing is doing. 
 # If you have difficulty, go into IRB and play with the methods.
+=begin
+
+  ALL MY COMMENTS ARE TO THE RIGHT OF EACH LINE OF CODE.
+  input = coded_message.downcase.split("") #this take the original string called coded_message, makes it all lower case and split each character into an array.
+  decoded_sentence = [] #this is declaring an empty array called decoded_sentence
+  cipher = {"e" => "a",   # This is technically a shift of four letters...Can you think of a way to automate this? Is a hash
+            "f" => "b",   # the best data structure for this problem? What are the pros and cons of hashes?
+            "g" => "c",   #Best daata structure would be an array since we know that the cipher is just shifting the four letters backwards.
+            "h" => "d",   #A hash would work good here if the cipher has a random assignemnt
+            "i" => "e",   #The best way to automate this is by setting up an array called cipher
+            "j" => "f",   #example: cipher = ('a'..'z').to_a
+            "k" => "g",   #Then take the cipher array and use it to move letters back 4 slots based on the character taken in from the input array.
+            "l" => "h",
+            "m" => "i",
+            "n" => "j",
+            "o" => "k",
+            "p" => "l",
+            "q" => "m",
+            "r" => "n",
+            "s" => "o",
+            "t" => "p",
+            "u" => "q",
+            "v" => "r",
+            "w" => "s",
+            "x" => "t",
+            "y" => "u",
+            "z" => "v",
+            "a" => "w",
+            "b" => "x",
+            "c" => "y",
+            "d" => "z"}
+  input.each do |x| # each is iterating through each character from the original string 
+    found_match = false  # this is assigned false because it is used at the bottom of the code if the current character being compared doesnt match any of the conditions
+                         # If found_match was true, it will do nothing and the current character wont get added to the final decoded_sentence
+    cipher.each_key do |y| # #each_key is looking in the cipher hash for the key that matches the current character being compared (variable x)
+      if x == y  # This is comparing if the current character taken from the input array matches with the hash key.  X is coming from the input.each iteration call
+                 # y is coming from the cipher.each_key iteration call.  Both variables are string characters stored in an array.
+        puts "I am comparing x and y. X is #{x} and Y is #{y}."
+        decoded_sentence << cipher[y]
+        found_match = true
+        break  # Why is it breaking here?  The break takes you out of the cipher.each_key iteration call when it found a match.
+      elsif x == "@" || x == "#" || x == "$" || x == "%"|| x == "^" || x == "&"|| x =="*" #This is comparing if any of this symbols match, pass a blank space to the decoded_sentence
+        decoded_sentence << " "
+        found_match = true
+        break
+      elsif (0..9).to_a.include?(x) # This builds an array of integers from 0 to 9 and include? is comparing if the x value is a number between 0 to 9. If it is a number, return true.
+        decoded_sentence << x
+        found_match = true
+        break
+      end 
+    end
+    if not found_match  # This just checks that if the current x value is not a match for any of the above comparison, then just pass itself to the decoded_sentence
+      decoded_sentence << x
+    end
+  end
+  decoded_sentence = decoded_sentence.join("")
+ 
+  if decoded_sentence.match(/\d+/) #This is trying to use regular expression to match up one or more digits 
+    decoded_sentence.gsub!(/\d+/) { |num| num.to_i / 100 } #He's been known to exaggerate...Yes....we take the number found and divide by 100 to chop off two zeros from the end such that 10000 =
+  end
+  return decoded_sentence # This is returning a string which is the final decoded sentence.   The decoded sentence array was joined up to become a string of all the characters together.
+end
+=end
 
 
-
+=begin
+#THIS IS THE ORIGINAL CODE
 def north_korean_cipher(coded_message)
   input = coded_message.downcase.split("") # Check out this method in IRB to see how it works! Also refer to the ruby docs.
   decoded_sentence = []
@@ -70,21 +131,35 @@ def north_korean_cipher(coded_message)
   end  
   return decoded_sentence # What is this returning?        
 end
+=end
 
 # Your Refactored Solution
-
-
-
-
+def north_korean_cipher(coded_message) 
+  input = coded_message.downcase.split("") 
+  decoded_sentence = []
+  cipher = ('a'..'z').to_a
+  input.each do |x| 
+    if cipher.index(x) != nil
+      decoded_sentence << cipher[cipher.index(x)-4]
+    elsif x == "@" || x == "#" || x == "$" || x == "%"|| x == "^" || x == "&"|| x =="*"
+      decoded_sentence << " "
+    else
+      decoded_sentence << x 
+    end 
+  end
+  decoded_sentence = decoded_sentence.join("")
+  if decoded_sentence.match(/\d+/) 
+      decoded_sentence.gsub!(/\d+/) { |num| num.to_i / 100 }
+  end  
+  return decoded_sentence
+end
 
 # Driver Code:
 p north_korean_cipher("m^aerx%e&gsoi!") == "i want a coke!" #This is driver code and should print true
 # Find out what Kim Jong Un is saying below and turn it into driver code as well. Driver Code statements should always return "true"
-p north_korean_cipher("syv@tistpi$iex#xli*qswx*hipmgmsyw*erh*ryxvmxmsyw%jsshw^jvsq^syv#1000000#tvsjmxefpi$jevqw.")
-p north_korean_cipher("syv%ryoiw#evi#liph^xskixliv@fc^kveti-jpezsvih@xsjjii.*hsr'x%xipp&xli#yw!")
-p north_korean_cipher("mj^csy&qeoi^sri*qmwxeoi,%kir.*vm@csrk-kmp,&csy^ampp*fi&vitpegih*fc@hirrmw&vshqer.")
-p north_korean_cipher("ribx^wxst:$wsyxl%osvie,$xlir$neter,#xlir%xli%asvph!")
-p north_korean_cipher("ger^wsqifshc*nywx^kix^qi&10000*fekw@sj$gssp%vergl@hsvmxsw?")
-
-# Reflection
- 
+p north_korean_cipher("syv@tistpi$iex#xli*qswx*hipmgmsyw*erh*ryxvmxmsyw%jsshw^jvsq^syv#1000000#tvsjmxefpi$jevqw.") == "our people eat the most delicious and nutritious foods from our 10000 profitable farms."
+p north_korean_cipher("syv%ryoiw#evi#liph^xskixliv@fc^kveti-jpezsvih@xsjjii.*hsr'x%xipp&xli#yw!") == "our nukes are held together by grape-flavored toffee. don't tell the us!"
+p north_korean_cipher("mj^csy&qeoi^sri*qmwxeoi,%kir.*vm@csrk-kmp,&csy^ampp*fi&vitpegih*fc@hirrmw&vshqer.") == "if you make one mistake, gen. ri yong-gil, you will be replaced by dennis rodman."
+p north_korean_cipher("ribx^wxst:$wsyxl%osvie,$xlir$neter,#xlir%xli%asvph!") == "next stop: south korea, then japan, then the world!"
+p north_korean_cipher("ger^wsqifshc*nywx^kix^qi&10000*fekw@sj$gssp%vergl@hsvmxsw?") == "can somebody just get me 100 bags of cool ranch doritos?"
+#Reflection
