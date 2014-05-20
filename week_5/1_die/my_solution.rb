2293730f48bfa362eea8df9ec67f1880699cbaab
Tony Leung <tleung999@gmail.com>
2014-05-20 15:23:53 -0700

    completed challenge 1 and 2

--- week_5/1_die/my_solution.rb
+++ week_5/1_die/my_solution.rb
@@ -1,45 +1,90 @@
 # U2.W5: Die Class 1: Numeric
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: Create a new instace of Die, call method .sides, call method .roll
+# Output: It can output the number of sides and a random roll of the die based on the number of sides 
 # Steps:
-
+=begin
+Create a new class called Die
+Create an initialize method that takes in 1 argument called sides
+If the number of sides is greater than or equal to one. 
+	set sides equal to an instance variable called sides
+else if number is less than 1
+	raise an error 
+Create a method inside Die called sides
+	return the number of sides
+Create a method inside Die called roll
+	return a random number between 1 and the number of sides
+=end
 
 # 3. Initial Solution
-
+=begin
 class Die
   def initialize(sides)
-    # code goes here
+    
+    if sides >= 1 
+    	@sides = sides
+    else
+    	raise ArgumentError.new("Please enter the number of sides 1 or larger")
+    end
   end
   
   def sides
-    # code goes here
+    @sides
   end
   
   def roll
-    # code goes here
+    rand(@sides) + 1
+  end
+end
+=end
+# 4. Refactored Solution
+class Die
+  def initialize(sides)
+    @sides = sides
+    unless sides >= 1
+    	raise ArgumentError.new("Please enter the number of sides 1 or larger")
+    end
+  end
+  
+  def sides
+    @sides
+  end
+  
+  def roll
+    rand(@sides) + 1
   end
 end
 
 
-
-# 4. Refactored Solution
-
-
-
-
-
-
 # 1. DRIVER TESTS GO BELOW THIS LINE
+#Declare a new instance of Die
+d = Die.new(3)
 
+#Check to see if sides is returned correctly
+p d.sides == 3
 
+#Check to see if roll will produce a random number between 1 to 3
+p (1..3).include?(d.roll)
 
-
-
+#Check for an arguement error
+begin
+  Die.new(0)
+rescue ArgumentError => e
+  chkerror = e.message
+end 
+p chkerror == "Please enter the number of sides 1 or larger"
 
 # 5. Reflection 
+=begin
+So making sure I read up on Classes and how to create them was important
+in building my Class correctly. Also reading up on how to raise an exception 
+error was needed to write my ArgumentError correctly.  Only big question
+I had was how to write a driver code to check for an argument.    The rest of the 
+challenge was pretty easy.   Nothing was tedious.   I am very confident with the
+learning competencies.  
+=end
--- week_5/2_die/my_solution.rb
+++ week_5/2_die/my_solution.rb
@@ -1,42 +1,95 @@
 # U2.W5: Die Class 2: Arbitrary Symbols
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input: Create a instance of Die an pass in an array of non numbers, Can call the method sides, Can call the method roll
+# Output: Return the number of sides, Return a random string value randomly.
 # Steps:
+=begin
+Create a new class called Die
+Create an initialize method that takes in 1 argument called sides
+If the labels array is passed in 
+	set labels equal to an instance variable called sides
+else if labels array is not passed in
+	raise an error 
+Create a method inside Die called sides
+	return the number of sides
+Create a method inside Die called roll
+	return a random string value
+=end
 
 
 # 3. Initial Solution
 
 class Die
+
   def initialize(labels)
+  		if labels.empty?
+  			raise ArgumentError.new ("Array is empty!")
+  		end
+	  	@labels = labels
   end
 
   def sides
+  	@labels.length
   end
 
   def roll
+  	@labels[rand(@labels.length)]
   end
 end
 
-
-
 # 4. Refactored Solution
+#I couldnt see where I can make the code tighter and cleaner, so I didnt refactored.   
+class Die
 
+  def initialize(labels)
+  		if labels.empty?
+  			raise ArgumentError.new ("Array is empty!")
+  		end
+	  	@labels = labels
+  end
 
+  def sides
+  	@labels.length
+  end
 
-
-
+  def roll
+  	@labels[rand(@labels.length)]
+  end
+end
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+#Declare a new instance of Die
+d = Die.new(['A', 'B', 'C', 'D', 'E', 'F'])
 
+#Check to see if number of sides is correct
+p d.sides == 6
 
+#Check to see if roll will generate a random letter between A to F
+p ('A'..'F').include?(d.roll)
 
+#Check to see if Exception is thrown when you pass in blank array
+begin
+  Die.new([])
+rescue ArgumentError => e
+  chkerror = e.message
+end 
+p chkerror == "Array is empty!"
 
+#Check to see if Exception is thrown you dont pass in any arguments (This is for fun, my personal learning)
+begin
+  Die.new()
+rescue Exception => e
+  error_object = e.inspect
+end
+p error_object == "#<ArgumentError: wrong number of arguments (0 for 1)>"
 
 
 # 5. Reflection 
+=begin
+Taking what I learned from Challenge 1 and moving it to this challenge was very helpful.   Changing from numbers to strings wasnt that hard to deal with since I only need to figure out the length of the array to randomize and then use that number as the index of the array.    The only thing I got stuck with was simply that I didnt read the directions clearly.  I thought the instruction said to catch the exception error if you pass no argument when creating the instance of the class.   But it turned out that I need to pass in an empty array.    So checking to see if the array is empty helped catch the error and produce my message to the user.   I am now starting to know how to use begin, rescue, and end for an exception block.   Its a little weird in the beginning, but I think I'll get better once I use it more.  I didnt find the challenge tedious, it was a good learning experience.	
+=end
