d449a6a7068a987136d8d65dd0296e5981323051
Tony Leung <tleung999@gmail.com>
2014-05-27 22:28:41 -0700

    complete challenge 3

--- week_6/2_drawer_debugger/my_solution.rb
+++ week_6/2_drawer_debugger/my_solution.rb
@@ -1,73 +1,77 @@
 # U2.W6: Drawer Debugger
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 
 # 2. Original Code
 
 class Drawer
 
-attr_reader :contents
+	attr_reader :contents
 
-# Are there any more methods needed in this class?
+	# Are there any more methods needed in this class?  I dont think we need another method, but we never use the open or close method.
 
-def initialize
-@contents = []
-@open = true
+	def initialize
+		@contents = []
+		@open = true
+	end
+
+	def open
+		@open = true
+	end
+
+	def close
+		@open = false
+	end 
+
+	def add_item(item)
+		@contents << item
+	end
+
+	def remove_item(item = @contents.pop) #what is `#pop` doing? Pop is pulling out the last item on the stack. Which at this moment is a fork.
+		@contents.delete(item)
+	end
+
+	def dump  # what should this method return? Should return an empty array of objects.
+		puts "Your drawer is empty."
+		@contents = []
+	end
+
+	def view_contents
+		puts "The drawer contains:"
+		@contents.each {|silverware| puts "- " + silverware.type }
+	end
 end
 
-def open
-@open = true
-end
-
-def close
-@open = false
-end 
-
-def add_item
-@contents << item
-end
-
-def remove_item(item = @contents.pop) #what is `#pop` doing?
-@contents.delete(item)
-end
-
-def dump  # what should this method return?
-puts "Your drawer is empty."
-end
-
-def view_contents
-puts "The drawer contains:"
-@contents.each {|silverware| puts "- " + silverware.type }
-end
 
 
 class Silverware
-attr_reader :type
+	attr_reader :type, :clean
 
-# Are there any more methods needed in this class?
+	# Are there any more methods needed in this class?  Yes I created a new method called clean_silverware that will set @clean to true.
+	def initialize(type, clean = true)
+		@type = type
+		@clean = clean
+	end
 
-def initialize(type, clean = true)
-@type = type
-@clean = clean
-end
+	def eat
+		puts "eating with the #{type}"
+		@clean = false
+	end
 
-def eat
-puts "eating with the #{type}"
-@clean = false
-end
-
+	def clean_silverware
+		puts "Cleaning Silverware"
+		@clean = true
+	end
 end
 
 knife1 = Silverware.new("knife")
-
 silverware_drawer = Drawer.new
 silverware_drawer.add_item(knife1) 
 silverware_drawer.add_item(Silverware.new("spoon"))
 silverware_drawer.add_item(Silverware.new("fork")) 
 silverware_drawer.view_contents
-
 silverware_drawer.remove_item
 silverware_drawer.view_contents
 sharp_knife = Silverware.new("sharp_knife")
@@ -79,24 +83,62 @@ silverware_drawer.view_contents
 
 removed_knife = silverware_drawer.remove_item(sharp_knife)
 removed_knife.eat
-removed_knife.clean_silverware 
+removed_knife.clean_silverware
 
 silverware_drawer.view_contents
 silverware_drawer.dump
-silverware_drawer.view_contents #What should this return?
-
-
-fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...
+silverware_drawer.view_contents #What should this return? Should return a blank array.
+fork = Silverware.new("fork")
+silverware_drawer.add_item(fork)
+fork = silverware_drawer.remove_item(fork) #add some puts statements to help you trace through the code...  I had to create a new instance of Silverware for a fork and add the fork object to the silverware_drawer object.
 fork.eat
+fork.clean_silverware
 
 #BONUS SECTION
-# puts fork.clean
+puts fork.clean  #I added clean as an attr_reader to get the value of clean.   
 
 # DRIVER TESTS GO BELOW THIS LINE
+def assert
+  raise "Assertion failed!" unless yield
+end
 
+testdrawer = Drawer.new
+k = Silverware.new("knife")
+f = Silverware.new("fork")
+s = Silverware.new("spoon")
+testdrawer.add_item(k)
+testdrawer.add_item(f)
+testdrawer.add_item(s)
+ 
+#Test that the total objects are 3
+assert { testdrawer.contents.length  == 3 }
 
+#Test that the last object is a spoon
+remove_s = testdrawer.remove_item(s)
+assert { remove_s.type  == "spoon" }
 
+#Test that taking a silverware out to eat makes it dirty
+remove_s.eat
+assert { remove_s.clean == false}
 
+#Test that the silverware is clean after running the clean method.
+remove_s.clean_silverware
+assert { remove_s.clean == true }
 
+#Test that the emptying a drawer returns an empty array after using the method dump.
+testdrawer.dump
+assert { testdrawer.contents.empty? == true }
+
+#Writing 3 more method calls
+testdrawer.view_contents 	#should contain nothing
+testdrawer.open						#Open drawer
+testdrawer.add_item(k)		#add a knife
+testdrawer.view_contents	#show contents which is just a knife
+testdrawer.close					#close drawer
+testdrawer.remove_item		#remove the knife
+testdrawer.view_contents	#should contain nothing
 
 # 5. Reflection 
+=begin
+	This was an interesting challenge.  I never worked with passing in an object as an argument so it was a fun experience.   It does get a little confusing because passing in an object you then have to understand that the object also contain multiple methods and variables that you have to manage in your head.  I was not sure if I did this assignment absolutely correct.  The instruction on the challenge mentioned adjusting and adding in methods to the class.   So my initial thought was that we can ONLY change things inside the class, but in order for me to make the fork.eat method to work is if I created a new instance of fork and added it the silverware_drawer instance.   I added the code into the area outside of the class to get it all working.   The assert driver code wasnt too bad.   I basically wrote out comments on what I want to test and then I wrote the test code to see if it will return true.    I then also made the test equal to the opposite to make sure it is throwing the assert error.   I am slowly learning more about how to work with objects and attr.  I felt fairly confident about the learning objectives.   
+=end
--- week_6/3_BoggleBoard/my_solution.rb
+++ week_6/3_BoggleBoard/my_solution.rb
@@ -1,40 +1,137 @@
 # U2.W6: Create a BoggleBoard Class
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 
 # 2. Pseudocode
+=begin
+	Create a class called BoggleBoard
+	Create a attr_reader to allow you to read the data in board
+	Create an initialize method that takes in 1 2D array and assign it to the instance variable @board
+	Create a create_word method that takes in a wildcard of arguments.   These arguments are array coordinates such as [0,3].
+	 	In the create_word method, map the letters of each coordinates into an array and then join them together to form the word.
+	Create a get_row method that takes in 1 argument that is an integer
+		return the row selected
+	Create a get_col method that takes in 1 argument that is an integer
+		Transpose the rows to columns on the board
+		return the col selected
 
-
+=end
 
 # 3. Initial Solution
 class BoggleBoard
- 
-  #your code here
- 
-end
- 
- 
-dice_grid = [["b", "r", "a", "e"],
-             ["i", "o", "d", "t"],
-             ["e", "c", "l", "r"],
-             ["t", "a", "k", "e"]]
- 
-boggle_board = BoggleBoard.new(dice_grid)
- 
+	attr_reader :board
 
+	def initialize(board)
+		@board = board
+	end
+
+	def create_word(*coords)
+    coords.map { |coord| @board[coord.first][coord.last]}.join("")
+	end
+ 
+ 	def get_row(row)
+  	board[row].join("")
+	end
+
+	def get_col(col)
+		transpose_board = board.transpose
+		transpose_board[col].join("")
+	end
+
+	def get_diagonal(startcord, endcord)
+		diagonal_word = []
+		
+		until (startcord == endcord) do
+				diagonal_word << @board[startcord.first][startcord.last]		
+				if startcord[0] > endcord[0]
+					startcord[0] -= 1
+				else
+					startcord[0] += 1
+				end
+
+				if startcord[1] > endcord[1]
+					startcord[1] -= 1
+				else
+					startcord[1] += 1
+				end
+		end
+		diagonal_word << @board[endcord.first][endcord.last]
+		diagonal_word.join("")
+	end
+
+end
 
 # 4. Refactored Solution
-
-
-
+=begin
+	I dont think there is anything I can do to make it more refactored.    I believe my solution is DRY and the
+	variables are named pretty well.  Also after adding in the get_diagonal method, I'm not exactly sure how to refactor the code.   I needed to deal with each array element individually, compare each piece to the end coordinates and then decide whether to increment by 1 or decrement by 1. 
+=end
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 # create driver test code to retrieve a value at a coordinate here:
 # implement tests for each of the methods here:
+dice_grid = [["b", "r", "a", "e"],
+             ["i", "o", "d", "t"],
+             ["e", "c", "l", "r"],
+             ["t", "a", "k", "e"]]
 
+boggle_board = BoggleBoard.new(dice_grid)
+
+def assert
+  raise "Assertion failed!" unless yield
+end
+
+#These coordinates create the word "dock"
+assert { boggle_board.create_word([1,2], [1,1], [2,1], [3,2])  == "dock" }
+
+#These coordinates create the word "code"
+assert { boggle_board.create_word([2,1], [1,1], [1,2], [0,3]) == "code"}
+
+#These coordinates create the word "rad"
+assert { boggle_board.create_word([0,1], [0,2], [1,2]) == "rad" }
+
+#Get row one - should return "iodt"
+assert { boggle_board.get_row(1) == "iodt" }
+
+#Get row three - should return "take"
+assert { boggle_board.get_row(3) == "take" }
+
+#Get column zero - should return "biet"
+assert { boggle_board.get_col(0) == "biet"}
+
+#Get column two - should return "adlk"
+assert { boggle_board.get_col(2) == "adlk"}
+
+#print out all rows and columns as strings
+4.times { |a| p boggle_board.get_row(a) }
+4.times { |b| p boggle_board.get_col(b) }
+#Results - Only real word is "take"
+# "brae"
+# "iodt"
+# "eclr"
+# "take"
+# "biet"
+# "roca"
+# "adlk"
+# "etre"
+
+#Access an individual coordinate
+assert { boggle_board.create_word([3,2]) == "k" }
+
+#Return diagonal word "edct"
+assert { boggle_board.get_diagonal([0,3], [3,0]) == "edct" }
+
+#Return diagonal word "ick"
+assert { boggle_board.get_diagonal([1,0], [3,2]) == "ick" }
+
+#Return diagonal word "ta"
+assert { boggle_board.get_diagonal([1,3], [0,2]) == "ta" }
 
 # 5. Reflection 
+=begin
+	Most of the challenge wasnt very difficult, the tough part of this challenge was the get_diagonal method.   I had to first break out the start coordinate into the row index, compare it to the end row index to figure out whether to increment or decrement the row index.   Then take the column index, compare it the end column index to figure out whether to increment or decrement the column index.    I also had problems trying to figure out how to throw an error if the diagonal coordinates isnt a diagonal.   The application will automatically throw a NoMethodError if it moves outside of its grid boundaries.   I am still trying to learn how to hand exceptions correctly and how to raise and rescue errors.   This was a fun exercise and I learn how to build the asserts better.
+=end
